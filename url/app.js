92856f67dca1cd69891e8211957b98c81b0de4f2
dhruvimadlani29 <dhruvimadlani2902@gmail.com>
2025-01-27 19:51:59 -0500

    Send url data and display it in front end

--- url/app.js
+++ url/app.js
@@ -1,34 +1,76 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 import { createServer } from 'http';
 import path from 'path';
+import crypto from 'crypto';
 
 const PORT = 3000;
+const DATA_FILE = path.join('data', 'links.json');
+
+const serverFile = async (res, fileURLToPath, contentType) => {
+  try {
+    const data = await readFile(fileURLToPath);
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  } catch (error) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 page not found');
+  }
+};
+
+const loadLinks = async () => {
+  try {
+    const data = await readFile(DATA_FILE, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await writeFile(DATA_FILE, JSON.stringify({}));
+      return {};
+    }
+    throw error;
+  }
+};
+
+const saveLinks = async (links) => {
+  await writeFile(DATA_FILE, JSON.stringify(links));
+};
 
 const server = createServer(async (req, res) => {
   if (req.method === 'GET') {
     console.log('req.url:', req.url);
 
-    try {
-      if (req.url === '/') {
-        const data = await readFile(path.join('public', 'index.html'));
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      } else if (req.url === '/style.css') {
-        const data = await readFile(path.join('public', 'style.css'));
-        res.writeHead(200, { 'Content-Type': 'text/css' });
-        res.end(data);
-      } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('404 page not found');
-      }
-    } catch (err) {
-      console.error('Error reading file:', err.message); // Log the error message
-      res.writeHead(500, { 'Content-Type': 'text/html' });
-      res.end('500 Internal Server Error');
+    if (req.url === '/') {
+      return serverFile(res, path.join('public', 'index.html'), 'text/html');
+    } else if (req.url === '/style.css') {
+      return serverFile(res, path.join('public', 'style.css'), 'text/css');
+    } else if (req.url === '/links') {
+      const links = await loadLinks();
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(links));
     }
-  } else {
-    res.writeHead(405, { 'Content-Type': 'text/html' });
-    res.end('405 Method Not Allowed');
+  }
+  if (req.method === 'POST' && req.url === '/shorten') {
+    const links = await loadLinks();
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+    req.on('end', async () => {
+      console.log(body);
+      const { url, shortCode } = JSON.parse(body);
+      if (!url) {
+        res.writeHead(400, { 'Content-Type': 'test/plain' });
+        return res.end('URL is required');
+      }
+      const finalShortCode = shortCode || crypto.randomBytes(4).toString('hex');
+      if (links[finalShortCode]) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        return res.end('Short code already exists. Please choose  another');
+      }
+      links[finalShortCode] = url;
+      await saveLinks(links);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      return res.end(JSON.stringify({ shortCode: finalShortCode }));
+    });
   }
 });
 
